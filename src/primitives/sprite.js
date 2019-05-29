b76d481e67e222289b8d5c3c7d665d70563e128b
Joon Suh <joon.suh2003@gmail.com>
2019-05-29 10:03:04 +1000

    (mostly) finish sprite support

--- src/primitives/sprite.js
+++ src/primitives/sprite.js
@@ -10,12 +10,12 @@ import Node from "../primitives/node.js"
  * @param {Vector}  options.size         Size Vector, defaults to whatever size the image source is
  * @param {Object=} options.region       The section of the image to draw
  * @param {Vector=} options.region.begin Top left corner of the clipping region
- * @param {Vector=} options.region.end   Bottom right corner of the clipping region
+ * @param {Vector=} options.region.size  Size of the clipping region
  */
-class Sprite extends Node {
-    size = new Vector
+class   Sprite extends Node {
     constructor(options) {
         super(options)
+        this.size = this.size || new Vector
         this.img = new Image(this.size.x, this.size.y);
         this.img.src = options.src;
     }
@@ -23,7 +23,7 @@ class Sprite extends Node {
         if(!this.region) {
             ctx.drawImage(this.img, 0, 0, this.size.x || this.img.naturalWidth, this.size.y || this.img.naturalHeight);
         } else {
-
+            ctx.drawImage(this.img, this.region.begin.x, this.region.begin.y, this.region.size.y, this.region.size.y, 0, 0, this.size.x || this.region.size.x, this.size.y || this.region.size.y);
         }
     }
 }
