1587fa61dd50bd33a8263ef04be7b2afb01feff3
Joon Suh <joon.suh1@education.nsw.gov.edu.au>
2019-06-11 14:23:38 +1000

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    devtool: "sourcemap",
+    entry: {
+        flap: './src/flap.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/dist/"
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-class-properties']
+              }
+            }
+          }
+        ]
+      }
+};
