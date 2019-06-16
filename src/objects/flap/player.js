767d3540ddc12e1d7bbdf5424aa829cf9f6e13e9
Joon Suh <joon.suh1@education.nsw.gov.edu.au>
2019-06-17 09:57:26 +1000

    Hoverbored sprites

--- src/objects/flap/player.js
+++ src/objects/flap/player.js
@@ -10,7 +10,7 @@ export default class Player extends Entity {
 
     alive = true;
 
-    children = [new Sprite({
+    player = new Sprite({
         src: "/assets/flap/flap.png",
         size: new Vector(64, 64),
         position: new Vector(-32, -32),
@@ -18,9 +18,43 @@ export default class Player extends Entity {
             begin: new Vector,
             size: new Vector(128, 128)
         }
-    })]
+    });
+
+    hoverboardSprites = new Sprite({
+        src: '/assets/hoverbored/coolguy.png',
+        size: new Vector(32, 34),
+        position: new Vector(-16, -23),
+        visible: false,
+        region: {
+            begin: new Vector(0, 0),
+            size: new Vector(64, 68)
+        },
+        children: [new Entity({
+            position: new Vector(16, 17),
+            children: [
+                new Sprite({
+                    src: "/assets/hoverbored/hand.png",
+                    position: new Vector(22, -4),
+                    size: new Vector(36 * 0.75, 18 * 0.75),
+                }),
+                new Sprite({
+                    src: "/assets/hoverbored/hand2.png",
+                    position: new Vector(-49, -4),
+                    size: new Vector(27, 13),
+                }),
+                new Sprite({
+                    src: "/assets/hoverbored/hoverboard.png",
+                    position: new Vector(-58.75/2, 24),
+                    size: new Vector(47*1.25, 16),
+                })
+            ]
+        })]
+    })
+
+    children = [this.player, this.hoverboardSprites]
 
     size = new Vector(0, 0); // Hitbox size
+    const 
 
     state = 0;    // Player state, 0: Flappy Bird, 1: Copter
 
@@ -50,6 +84,9 @@ export default class Player extends Entity {
 
         // Flappy Bird
         if (this.state == 0){
+            this.children[0].visible = true;
+            this.hoverboardSprites.visible = false;
+
             this.v.y += gravity * delta;
             if (this.parent.jumpJustPressed) {
                 this.v.y = -600;
@@ -61,7 +98,10 @@ export default class Player extends Entity {
         }
         // Copter
         else if (this.state == 1) {
+            this.children[0].visible = false;
+            this.hoverboardSprites.visible = true;
             this.v.y = this.game.keys.Space ? -400 : 400;
+            this.hoverboardSprites.children[0].rotation += ((this.game.keys.Space ? -0.3 : 0.3) - this.hoverboardSprites.children[0].rotation) / 5
         }
 
         // Clamp y velocity
