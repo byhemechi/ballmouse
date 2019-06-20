ddcf02d6ae2175f813583cd922baab46f8f95c95
Joon Suh <joon.suh1@education.nsw.gov.edu.au>
2019-06-20 20:02:52 +1000

    some Docs

--- src/objects/stopboat/weapon.ts
+++ src/objects/stopboat/weapon.ts
@@ -1,24 +1,34 @@
 import { Vector } from "../../types"
 
-/**
- * A container class that stores weapon data.
- * @param {Object}  options                 Options for the class; see below
- * @param {number}  options.damage          How much damage the bullets do
- * @param {number}  options.speed           Speed of bullets
- * @param {number}  options.spread          Spread of bullets in radians
- * @param {bool}    options.canFire         Flag if we can fire
- * @param {number}  options.queue           How many bullets left to fire
- * @param {number}  options.firerate        Time between each bullet in seconds
- * @param {number}  options.timer           Timer for how long it has been since we shot
- * @param {number}  options.magsize         Size of magazine
- * @param {number}  options.reloadtime      Time it takes to reload in seconds
- * @param {number}  options.burstcount      How many bullets per shot, default to 1
- * @param {number}  options.burstfirerate   Time between each burst shot in seconds
- * @param {number}  options.bursttimer      Timer for how long it has been since last bullet fired
- * 
- */
+interface WeaponOptions {
+    /** How much damage the bullets do */
+    damage: number;
+    /** Speed of bullets */
+    speed: number;
+    /** Spread of bullets in radians */
+    spread: number;
+    /** Flag if we can fire */
+    canFire: boolean;
+    /** How many bullets left to fire */
+    queue: number;
+    /** Time between each bullet in seconds */
+    firerate: number;
+    /** Timer for how long it has been since we shot */
+    timer: number;
+    /** Size of magazine */
+    magsize: number;
+    /** Time it takes to reload in seconds */
+    reloadtime: number;
+    /** How many bullets per shot, default to 1 */
+    burstcount: number;
+    /** Time between each burst shot in seconds */
+    burstfirerate: number;
+    /** Timer for how long it has been since last bullet fired */
+    bursttimer: number;
+}
 
 export default class Weapon {
+    damage: number;
     speed: number;
     spread: number;
     canFire: boolean = true;
