8f66e1b778d16e9e213c50fca612d0f41ec4ed4f
Joon Suh <joon.suh1@education.nsw.gov.edu.au>
2019-07-11 14:55:51 +1000

    oops

--- src/objects/stopboat/boat.ts
+++ src/objects/stopboat/boat.ts
@@ -16,7 +16,7 @@ export default class Boat extends Entity {
         speed: 500,
         damage: 3,
         spread: 0.005,
-        firerate: 0.1,
+        firerate: 1,
     })
 
     size = new Vector(64,32);
